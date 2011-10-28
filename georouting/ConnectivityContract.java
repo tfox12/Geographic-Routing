23f60e865277e917b24af4e0f03621a63e098363
Tim Fox <tfox12@kent.edu>
2011-10-28 19:41:15 -0400

    forgot to add this! changed the contract to have correct parameters for node calculation

--- georouting/ConnectivityContract.java
+++ georouting/ConnectivityContract.java
@@ -5,5 +5,5 @@ import java.util.ArrayList
 public interface ConnectivityContract
 {
   public ArrayList<Edge> constructionConnectivity( ArrayList<Nodes> nodes);
-  public int densityToNumberOfNodes(double density);
+  public int densityToNumberOfNodes(double density, double width, double height);
 }
